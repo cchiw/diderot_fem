0d2de18922ca8c2506042719e5271bcfd1da9956
wraith1995 <teocollin1995@gmail.com>
2017-08-13 01:04:03 -0500

    fixed inability to find teem related files

--- programs/mesh_d2s_single/run.sh
+++ programs/mesh_d2s_single/run.sh
@@ -1,3 +1,3 @@
 make clean
 make
-make mesh_d2s_single_init.o
+g++  -O3 -fPIC -shared -I../util -I/opt/local -I/usr/local/  -I/home/teodoro/gitcode/again/femprime/src/lib/include   -o mesh_d2s_single_init.so mesh_d2s_single_init.c mesh_d2s_single.o  -Wl,--whole-archive -lteem -Wl,--no-whole-archive
