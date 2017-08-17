19633f23b7ae9487e533f2d11b6e89d2d089a861
wraith1995 <teocollin1995@gmail.com>
2017-08-17 18:11:39 -0500

    corresponding changes to naming conventions implemented

--- lib/makejson.py
+++ lib/makejson.py
@@ -9,25 +9,9 @@ import itertools
 
 
 def makejson(V,filename):
-#Example input:
-#Taken from https://stackoverflow.com/questions/14264431/expanding-algebraic-powers-in-python-sympy but useless
-# def pow_to_mul(expr):
-#     """
-#     Convert integer powers in an expression to Muls, like a**2 => a*a.
-#     """
-#     pows = list(expr.atoms(sp.Pow))
-#     if any(not e.is_Integer for b, e in (i.as_base_exp() for i in pows)):
-
-#         raise ValueError("A power contains a non-integer exponent")
-#     #repl = zip(pows, (Mul(*list([b]*e for b, e in i.as_base_exp()), evaluate=False) for i in pows))
-#     repl = zip(pows, (sp.Mul(*[b]*e,evaluate=False) for b,e in (i.as_base_exp() for i in pows)))
-#     return expr.subs(repl)
-
-
-
-#Because python uses ** for float exponentiation and because numpy tries to translate this into an absurdly slow pow
-#we need to manually replace all the pow(x,int) -> x*x*x... 
-#so we bring out the regex expressions.
+    #Because python uses ** for float exponentiation and because numpy tries to translate this into an absurdly slow pow
+    #we need to manually replace all the pow(x,int) -> x*x*x... 
+    #so we bring out the regex expressions.
     search = re.compile("k\[\d+\]\*\*\d+")
     def replace_func(match):
         string = match.group(0)
@@ -36,15 +20,11 @@ def makejson(V,filename):
         return(newstring)
     def replace_powers(string):
         return(re.sub("k\[\d+\]\*\*\d+",replace_func,string))
-
     def replace_func_2(match):
         string = match.group(0)
         return("("+string+")")
-
-
     def float_point_protect(string):
         return(re.sub("-?\d+\.\d+e-\d+",replace_func_2,string))
-
 
     #we are going to deal with a lot of multidimensional arrays in the future and this is the best way to deal with it
     def makeIndices(shape):
@@ -55,9 +35,7 @@ def makejson(V,filename):
             here = [(x,) for x in range(0,shape[0])]
             return([a+b for a in here for b in rest])
 
-
-
-        #for use later:
+        #take a symp expression and turn it into a string that we care about
         def stringFunc(x):
             return(float_point_protect(replace_powers(str((sp.expand(x))))))
 
@@ -73,7 +51,11 @@ def makejson(V,filename):
             
 
         dim = len(mesh.coordinates.dat.data[0]) #dimension of ground space
-        nnodes = len(V.mesh().coordinates.cell_node_map().values[0]) #number of nodes per element
+        #dimension of geometric function space -> number of nodes for changes of coordinates:
+        gdim = len(V.mesh().coordinates.cell_node_map().values[0])
+        #dimension of the function space -> number of basis functions
+        sdim = len(V.coordinates.cell_node_map().values[0])
+        print("The dimension is {0} with gdim {1} and sdim {2}".format(dim,gdmim,sdim))
 
 
         #geometric basis functions:
@@ -83,27 +65,22 @@ def makejson(V,filename):
         basis1 = np.array(rf.tabulate(0, np.array(symbols))[(0,0)])
         gemBasis = applyStringFunc(basis1).flatten().tolist()
 
-
-
         #geometric first derivative:
         basis2 = (rf.tabulate(1, np.array(symbols)))
         geometricJacobian = (np.hstack((basis2[(1,0)],basis2[(0,1)]))) #nnodes x dim
         geometricJacobian = applyStringFunc(geometricJacobian).tolist()
 
 
-
         #that one point k that is in the "middle" of the reference element:
         vertex = np.array(rf.get_reference_element().get_vertices())
         centerOfRefCell = map(float,np.average(vertex, axis=0).tolist())
 
-        #test:
+        #test that we need to see if we are in the refrence element
         point = tuple(sp.Symbol("newpos[%d]" % i) for i in xrange(dim))
         #credit to this one goes to the author of the diderot integration branch of firedrake:
         test = " && ".join("(%s)" % arg for arg in rf.get_reference_element().contains_point(point,epsilon=1e-14).args)
 
-        # # #isAffine = cord_element.is_simplex() and cord_element.degree() <= 1 and cord_element.family() in ["Discontinuous Lagrange", "Lagrange"]
-        # # #FIX ME^
-        #credit as above:
+        #credit to Firedrake:
         degree = cord_element.degree() <= 1
         fam = cord_element.family() in  ["Discontinuous Lagrange", "Lagrange"]
         simplex = cord_element.cell().is_simplex()
@@ -116,13 +93,11 @@ def makejson(V,filename):
         symbols = [[sp.Symbol("k[%d]" % i) for i in xrange(V.mesh().topological_dimension())]]
         basis3 = (rf.tabulate(1, np.array(symbols)))
         temp = (basis3[(0,0)])
-        spaceBasis = applyStringFunc(temp).flatten().tolist() #some verification needs to happen here
 
-
+        spaceBasis = applyStringFunc(temp).flatten().tolist() 
         spaceJacobian = applyStringFunc(np.hstack((basis3[(1,0)],basis3[(0,1)]))).tolist()
 
-
-        result =json.dumps({"dim": dim, "nnodes" : nnodes, "test": test, "isAffine": isAffine, "k": centerOfRefCell, "basis_functions" : gemBasis, "basis_dervs" : {"1": geometricJacobian}, "eval_basis": spaceBasis, "eval_dervs": {"1": spaceJacobian}},sort_keys=False,indent=4)
+        result =json.dumps({"dim": dim, "gdim" : gdim, "sdim" : sdim, "test": test, "isAffine": isAffine, "k": centerOfRefCell, "g_basis_functions" : gemBasis, "g_basis_dervs" : {"1": geometricJacobian}, "s_basis_functions": spaceBasis, "s_basis_dervs": {"1": spaceJacobian}},sort_keys=False,indent=4)
 
         with open(filename,'w+') as f:
             f.write(result)
